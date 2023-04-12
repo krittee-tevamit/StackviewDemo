9c7bc10482eaa14bb8252437f843b22b5b922f31
krittee-tevamit <ktevamit@gmail.com>
2023-04-12 12:45:06 +0700

    Initial Commit

---
+++ StackviewDemo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StackviewDemo
+//
+//  Created by codinglife365 on 12/4/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StackviewDemo/StackviewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackviewDemoApp.swift
+//  StackviewDemo
+//
+//  Created by codinglife365 on 12/4/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct StackviewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
