32e69e455d8619a96998f10a1ee9bba8390ecb96
krittee-tevamit <ktevamit@gmail.com>
2023-04-12 15:42:45 +0700

    Initial Commit

--- StackviewDemo/ContentView.swift
+++ StackviewDemo/ContentView.swift
@@ -8,19 +8,131 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    
+    var body: some View {
+        VStack (spacing: 20) {
+            VStack {
+                Text("Instant Devloper")
+                    .fontWeight(.medium)
+                    .font(.system(size: 40))
+                    .foregroundColor(.indigo)
+                
+                Text("Get help from experts in 15 minutes")
+            }
+            
+            HStack (alignment: .bottom, spacing: 10) {
+                Image("user1")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("user2")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("user3")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding(.horizontal, 20)
+            
+            Text("Need help with coding problem? Resister!")
+            
+            
+            Spacer()
+            
+            //VSignUpButtonGroup()
+            if verticalSizeClass == .compact {
+                HSignUpButtonGroup()
+            } else {
+                VSignUpButtonGroup()
+            }
+            
+        }
+        .padding(.top, 30)
+       
+    }
+}
+
+
+struct VSignUpButtonGroup: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                
+            } label: {
+                Text("Sign Up")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.indigo)
+            .cornerRadius(10)
+            
+            
+            Button {
+                
+            } label: {
+                Text("Log in")
+            }
+            .frame(width: 200)
+            .padding()
+            .background(Color.gray)
+            .foregroundColor(Color.white)
+            .cornerRadius(10)
         }
-        .padding()
+    }
+}
+
+struct HSignUpButtonGroup: View {
+    var body: some View {
+        HStack {
+            Button {
+                
+            } label: {
+                Text("Sign Up")
+            }
+            .frame(width: 200)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.indigo)
+            .cornerRadius(10)
+            
+            
+            Button {
+                
+            } label: {
+                Text("Log in")
+            }
+            .frame(width: 200)
+            .padding()
+            .background(Color.gray)
+            .foregroundColor(Color.white)
+            .cornerRadius(10)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+                    ContentView()
+                        .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro"))
+                        .previewDisplayName("iPhone 12 Pro")
+
+                    ContentView()
+                        .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro"))
+                        .previewDisplayName("iPhone 12 Pro")
+                        .previewInterfaceOrientation(.landscapeLeft)
+
+                    ContentView()
+                        .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro Max"))
+                        .previewDisplayName("iPhone 12 Pro Max")
+
+                    ContentView()
+                        .previewDevice(PreviewDevice(rawValue: "iPad Air (4th generation)"))
+                        .previewDisplayName("iPad Air")
+                }
     }
 }
